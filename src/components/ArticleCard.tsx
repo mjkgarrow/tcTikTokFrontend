145e560487325f34de2ea4191065ba7f4b97cc47
Matt G <mjkgarrow@gmail.com>
2025-02-17 22:51:19 +1100

    edit: made shadow its own element

--- src/components/ArticleCard.tsx
+++ src/components/ArticleCard.tsx
@@ -42,8 +42,8 @@ export default function ArticleCard({
   return (
     <div
       ref={targetRef}
-      className="snap-start h-dvh md:p-14 place-content-center">
-      <div className="relative mx-auto bg-white h-full w-full overflow-hidden grid grid-cols-1 grid-rows-1 text-gray-50 md:aspect-square lg:aspect-[4/3] md:w-auto md:rounded-lg md:shadow-2xl after:absolute after:inset-0 after:bg-gradient-to-t after:from-black/50 after:to-transparent after:pointer-events-none">
+      className="snap-start h-dvh md:p-16 place-content-center">
+      <div className="mx-auto bg-white h-auto max-h-full w-auto max-w-full overflow-hidden grid grid-cols-1 grid-rows-1 max-md:h-full max-md:w-full md:aspect-[3/4] xl:aspect-[4/3] md:rounded-lg shadow-2xl text-gray-50">
         <figure className="col-start-1 row-start-1">
           <ResponsiveImage
             src={data.image}
@@ -51,7 +51,9 @@ export default function ArticleCard({
           />
         </figure>
 
-        <div className="col-start-1 row-start-1 p-4 pb-7 z-10 h-full flex flex-col justify-end">
+        <div className="col-start-1 row-start-1 h-full w-full bg-gradient-to-t from-black/50 to-transparent pointer-events-none"></div>
+
+        <div className="col-start-1 row-start-1 p-4 pb-7 h-full flex flex-col justify-end">
           <Author
             author={data.author}
             authorImg={data.authorImg}
